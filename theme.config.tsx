73f51de366b081fbd4d2c0a142562ba6f4c731a5
Kushagra <skushagra.sharma@gmail.com>
2024-04-14 23:45:44 +0530

    optimized for seo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span className="font-bold">
-    Neovim Essentials
-  </span>,
+  logo: (
+    <>
+      <span className="nx-font-extrabold nx-text-xl nx-tracking-tighter">Neovim Essentials</span>
+
+    </>
+  ), project: {
+    link: 'https://github.com/skushagra9/Neovim-Essentials',
+  },
+  chat: {
+    link: 'https://discordapp.com/channels/@me/kushagra_16/',
+  },
   editLink: { component: null },
   feedback: { content: null },
   footer: { component: null }
